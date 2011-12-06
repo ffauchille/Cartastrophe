ee88e07550140f778628c9af536b9d49554835aa
Coyf <coyf@slutofcoyf.(none)>
2011-12-06 19:22:23 +0100

    Modif makefile interface, projet compile pas

--- Makefile
+++ Makefile
@@ -1,14 +1,26 @@
 
 OCAML=ocamlc
 OCAMLFLAGS= -I +sdl -I +lablgtk2
-OCAMLLD= bigarray.cma lablgtk.cma sdl.cma sdlloader.cma 
+OCAMLLD= bigarray.cma lablgtk.cma gtkInit.cmo sdl.cma sdlloader.cma 
 BIN_NAME=cartastrophe
 SOURCES = imageProcessing.ml objMaker.ml interface.ml  main.ml
-all: compile
-compile:
-	${OCAML} ${OCAMLFLAGS} ${OCAMLLD} -o ${BIN_NAME} ${SOURCES}
+INTERFACES= ${SOURCES:.ml=.mli}
+.SUFFIXES:.ml .cmi .cmx .mli
+.ml.cmx: ${SOURCES}
+	${OCAML} ${OCAMLFLAGS} ${OCAMLLD} -c $<
+.ml.mli: ${SOURCES}
+	${OCAML} ${OCAMLFLAGS} ${OCAMLLD} -i $< > ${<:.ml=.mli}
+.mli.cmi: ${INTERFACES}
+	${OCAML} ${OCAMLFLAGS} ${OCAMLLD} -c $<
 
-clean:
-	rm -f *~ *.o *.cm? .
- 
+all: link
+link:compile
+	${OCAML} ${OCAMLFLAGS} ${OCAMLLD} -o ${BIN_NAME}
+compile:${INTERFACES} ${SOURCES}
+	${OCAML} ${OCAMLFLAGS} ${OCAMLLD} -c ${INTERFACES} ${SOURCES}
+clean::
+	rm -f *~ *.o *.cm? 
+usage:
+	echo "Usage: make compile|interface|clean|usage"
+end:
 # FIN
--- interface.ml
+++ interface.ml
@@ -1,22 +1,19 @@
-
 let width = ref 800
 let height= ref 600
 let namefile= ref "map-simple.png"
-let namefiletreated= ref "map-simple-traite.bmp"
+let namefiletreated= ref "map-simple.png-traite.bmp"
 let interval = ref 10
-
-(* On crÅÈe la surface d'affichage en doublebuffering *)
-(* let newDisplay () =(Sdlvideo.set_video_mode (!width) (!height) [`DOUBLEBUF])
+(* On cr¬Å√©e la surface d'affichage en doublebuffering *)
+(* let newDisplay () =(Sdlvideo.set_video_mode (!width) (!height) [`DOUBLEBUF]
 let display = ref (newDisplay ())
-
-let setSize w h = width:=w;height:=h;display :=newDisplay ()  *)
-let setSize w h = () 
-(* Affiche une image ‡ l'Ècran *)
+let setSize w h = width:=w;height:=h;display :=newDisplay () *)
+let setSize w h = ()
+(* Affiche une image √† l'√©cran *)
 (* let showSurface img = 
 	let d = Sdlvideo.display_format img in
 		Sdlvideo.blit_surface d !display ();
 		Sdlvideo.flip !display
-		 *)
+		 *)
 let setCaption = Sdlwm.set_caption
 (*Ouverture de la fenetre*)
 let window = GWindow.window
@@ -25,43 +22,58 @@ let window = GWindow.window
     ~height:!height
     ~width:!width ()
 (*Cadre principal*)
-
-
-
 (*Permettre l'ajout de widgets dans window*)
 let vbox = GPack.vbox
     ~spacing:5
     ~border_width:5
     ~packing:window#add ()
-
-let hbox = GPack.hbox
-    ~spacing:5 (* Les enfants sont espacÈs de 5 pixels. *)
-    ~border_width:5 (* La boÓte possËde une bordure de 5 pixels. *)
-    ~packing:vbox#add ()
-  
-
-let frame = GBin.frame
-    ~label: "Image treated"
-    ~packing:vbox#add ()
-let frame_image_treated = GBin.frame
-    ~label:"Image not treated"
-    ~packing:hbox#add ()
-(*Insertiona du scrolling*)
-let scroll = GBin.scrolled_window
-    ~height:200
-    ~hpolicy:`ALWAYS
-    ~vpolicy:`ALWAYS
-    ~packing:vbox#add ()
 (*emplacement boutons*)
 let bbox = GPack.button_box `HORIZONTAL
     ~layout:`SPREAD
     ~packing:(vbox#pack ~expand:false) ()
+let hbox = GPack.hbox
+    ~spacing:5 (* Les enfants sont espac√©s de 5 pixels. *)
+    ~border_width: 5 (* La bo√Æte poss√®de une bordure de 5 pixels. *)
+    ~packing:vbox#add ()
+let image_filter =GFile.filter
+    ~name:"Image file"
+    ~patterns:["*.bmp";"*.png";"*.jpg";"*.jpeg"]
+let open_button = GFile.chooser_button
+    ~title:"Map"
+    ~action:`OPEN
+    ~packing:bbox#add ()
+(*Cr√©ation des diff√©rents cadres*)
+let frame = GBin.frame
+    ~label: "Image not Processed"
+    ~packing:vbox#add ()
+let frame_image_treated = GBin.frame
+    ~label:"Image Processed"
+    ~packing:hbox#add ()
+(*Bouton d'aide*)
+let help =
+    let button= GButton.button
+	~label:"Help"
+	~packing:bbox#add () in
+    (*sw*) (button#connect#clicked ~callback:GMain.quit);
+    button
+(*Bouton quitter*)
+let quit =
+    let button = GButton.button
+	~label:"Quit"
+	~packing:bbox#add () in
+    (button#connect#clicked ~callback:GMain.quit);
+    button
+(*Insertiona du scrolling*)
+let scroll = GBin.scrolled_window
+    ~hpolicy:`ALWAYS
+    ~vpolicy:`ALWAYS
+    ~packing:window#add ()
 
 (*Creation d'une box pour l'image*)
 let imgbox = GPack.box `VERTICAL
-    ~spacing: 10
+    ~spacing: 5
     ~border_width: 5
-    ~packing: window#add ()
+    ~packing: window#add () 
     
 (*affichage de l'image d'un GMisc.image*)
 let imageview = GMisc.image
@@ -70,36 +82,20 @@ let imageview = GMisc.image
 let imagetreatedview = GMisc.image
     ~file:!namefiletreated
     ~packing:frame_image_treated#add ()
-    
+let imagetreated =  GMisc.drawing_area
+    ~height:5
+    ~width:5
+    ~packing:frame_image_treated#add ()
 (* Suppress warnings *)
 let sw foo = ()
-let help_message () = print_endline "Cliquez sur \"Quitter\" pour quitter"
-(*Bouton d'aide*)
-let help =
-    let button= GButton.button
-	~stock:`HELP
-	~packing:bbox#add () in
-    (*sw*) (button#connect#clicked ~callback:help_message);
-    button
-(*Bouton quitter*)
-let quit =
-    let button = GButton.button
-	~stock:`QUIT
-	~packing:bbox#add () in
-    (*sw*) (button#connect#clicked ~callback:GMain.quit);
-    button
 (* Initialisation SDL,GTK2 et ouverture de la fenetre *)
 let init () = 
 	begin
 		Sdl.init [`EVERYTHING];
-		Sdlevent.enable_events Sdlevent.all_events_mask;
-		
-		(*sw*) (GMain.init ());
+		(*Sdlevent.enable_events Sdlevent.all_events_mask;*)
+		GMain.init ();
 		(*sw*) (window#connect#destroy ~callback:GMain.quit);
 		window#show ();
-		GMain.main ()
+
+		GMain.Main.main ()
 	end
-
-
-
-
