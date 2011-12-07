fd14d571e85aa789a6761b2588ead0767c981855
Coyf <coyf@slutofcoyf.(none)>
2011-12-07 20:09:22 +0100

    Ajout bouton a propos avec acces au site en cliquant sur cartastrophe, la classe

--- interface.ml
+++ interface.ml
@@ -1,7 +1,7 @@
 let width = ref 800
 let height= ref 600
 let namefile= ref "map-simple.png"
-let namefiletreated= ref "map-simple.png-traite.bmp"
+let namefiletreated= ref "map-simple.png"
 let interval = ref 10
 (* On crée la surface d'affichage en doublebuffering *)
 (* let newDisplay () =(Sdlvideo.set_video_mode (!width) (!height) [`DOUBLEBUF]
@@ -24,68 +24,83 @@ let window = GWindow.window
 (*Cadre principal*)
 (*Permettre l'ajout de widgets dans window*)
 let vbox = GPack.vbox
-    ~spacing:5
-    ~border_width:5
+    ~border_width:1
+    ~height:((!height)/3)	
+    ~width:((!width)/3)
     ~packing:window#add ()
 (*emplacement boutons*)
-let bbox = GPack.button_box `HORIZONTAL
-    ~layout:`SPREAD
-    ~packing:(vbox#pack ~expand:false) ()
-let hbox = GPack.hbox
-    ~spacing:5 (* Les enfants sont espacés de 5 pixels. *)
-    ~border_width: 5 (* La boîte possède une bordure de 5 pixels. *)
+let hbox = GPack.hbox 
+    ~border_width:2
+    ~height:(2*(!height)/3) (* Les enfants sont espacés de 5 pixels. *)
+    ~width:(2*(!width)/3)
     ~packing:vbox#add ()
+let bbox = GPack.button_box `VERTICAL
+    ~layout:`SPREAD
+    ~packing:(hbox#pack ~expand:false) ()
 let image_filter =GFile.filter
     ~name:"Image file"
     ~patterns:["*.bmp";"*.png";"*.jpg";"*.jpeg"]
-let open_button = GFile.chooser_button
-    ~title:"Map"
-    ~action:`OPEN
-    ~packing:bbox#add ()
 (*Création des différents cadres*)
-let frame = GBin.frame
-    ~label: "Image not Processed"
-    ~packing:vbox#add ()
+(*let frame_image_origin = GBin.frame 
+    ~label:"Original Image"
+    ~packing:vbox#add ()*)
 let frame_image_treated = GBin.frame
-    ~label:"Image Processed"
+    ~label: "Image traitée"
+    ~width:((!width)/10)
+    ~packing:vbox#add ()
+let frame_visualisation = GBin.frame
+    ~label:"Visualisation"
     ~packing:hbox#add ()
 (*Bouton d'aide*)
+let open_button = GFile.chooser_button
+    ~title:"Choix de la carte"
+    ~action:`OPEN
+    ~packing:bbox#add ()
 let help =
     let button= GButton.button
-	~label:"Help"
+	~stock:`HELP
 	~packing:bbox#add () in
     (*sw*) (button#connect#clicked ~callback:GMain.quit);
     button
+let about_button =
+  let dlg = GWindow.about_dialog
+    ~authors:["Quatrographes (<quatrographes AT gmail DOT com>)"]
+    ~copyright:"Copyright © 2011-2012 Cartastrophe Project"
+    ~license:"GNU General Public License v3"
+    ~version:"42.42"
+    ~website:"http://cartastro0.wordpress.com/"
+    ~website_label:"Cartastrophe"
+    ~position:`CENTER_ON_PARENT
+    ~parent:window
+    ~destroy_with_parent:true () in
+  let btn = GButton.button 
+    ~stock:`ABOUT 
+    ~packing:bbox#add () in
+  GMisc.image ~stock:`ABOUT ~packing:btn#set_image ();
+  btn#connect#clicked (fun () -> ignore (dlg#run ()); dlg#misc#hide ());
+  btn
 (*Bouton quitter*)
 let quit =
     let button = GButton.button
-	~label:"Quit"
+	~stock:`QUIT
 	~packing:bbox#add () in
     (button#connect#clicked ~callback:GMain.quit);
     button
-(*Insertiona du scrolling*)
-let scroll = GBin.scrolled_window
-    ~hpolicy:`ALWAYS
-    ~vpolicy:`ALWAYS
-    ~packing:window#add ()
-
 (*Creation d'une box pour l'image*)
-let imgbox = GPack.box `VERTICAL
+(*let imgbox = GPack.box `VERTICAL
     ~spacing: 5
     ~border_width: 5
-    ~packing: window#add () 
+    ~packing: window#add () *)
     
 (*affichage de l'image d'un GMisc.image*)
 let imageview = GMisc.image
-    ~file:!namefile
-    ~packing:frame#add ()
-let imagetreatedview = GMisc.image
     ~file:!namefiletreated
     ~packing:frame_image_treated#add ()
-let imagetreated =  GMisc.drawing_area
-    ~height:5
-    ~width:5
+let imagetreatedview = GMisc.image
+    ~file:!namefile
     ~packing:frame_image_treated#add ()
+let imagetreated =  GMisc.drawing_area
+    ~packing:frame_visualisation#add ()
 (* Suppress warnings *)
 let sw foo = ()
 (* Initialisation SDL,GTK2 et ouverture de la fenetre *)
@@ -93,9 +108,8 @@ let init () =
 	begin
 		Sdl.init [`EVERYTHING];
 		(*Sdlevent.enable_events Sdlevent.all_events_mask;*)
-		GMain.init ();
+		(*GMain.init ();*)
 		(*sw*) (window#connect#destroy ~callback:GMain.quit);
 		window#show ();
-
-		GMain.Main.main ()
+		GMain.main ()
 	end
