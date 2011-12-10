918e4964b6ff69844541329e7d5e13ebd763c067
Quentin <q.godron@gmail.com>
2011-12-10 16:24:31 +0100

    Fridi ridi

--- fridi.ml
+++ fridi.ml
@@ -1,8 +1,7 @@
 let rtri = ref 0.0
-let rquad = ref 0.0
 
 let resizeGLScene ~width ~height =
-  let ok_height = 
+  let ok_height =
     if height = 0 then 1 else height in
 
   GlDraw.viewport 0 0 width ok_height;
@@ -10,7 +9,7 @@ let resizeGLScene ~width ~height =
   GlMat.mode `projection;
   GlMat.load_identity ();
   
-  GluMat.perspective ~fovy:45.0 
+  GluMat.perspective ~fovy:45.0
     ~aspect:((float_of_int width)/.(float_of_int ok_height)) ~z:(0.1, 100.0);
     
   GlMat.mode `modelview;
@@ -29,8 +28,8 @@ let initGL () =
   GlMisc.hint `perspective_correction `nicest
 module VertexMap = Map.Make(Int32)
 let drawMap area vmap flist =
-    let f i= 
-        let (c,vx) = 
+    let f i=
+        let (c,vx) =
         try
             VertexMap.find i vmap
         with Not_found -> ((0.,0.,0.),(0.,0.,0.))
@@ -62,7 +61,7 @@ let killGLWindow () =
 let display area width height vmap flist=
   GMain.Timeout.add ~ms:20 ~callback:
   begin fun () ->
-       drawGLScene area (); true
+       drawMap area vmap flist; true
   end;
   area#connect#display ~callback:(drawMap area vmap flist);
   area#connect#reshape ~callback:resizeGLScene;
@@ -73,39 +72,4 @@ let display area width height vmap flist=
       resizeGLScene ~width ~height
     end;
 
-
-let createGLWindow title width height bits fullscreen =
-  let w = GWindow.window ~title:title () in
-  w#connect#destroy ~callback:(fun () -> GMain.Main.quit (); exit 0);
-  w#set_resize_mode `IMMEDIATE;
-  let area = GlGtk.area [`DOUBLEBUFFER;`RGBA;`DEPTH_SIZE 16;`BUFFER_SIZE bits]
-      ~width:width ~height:height~packing:w#add () in
-  area#event#add [`KEY_PRESS];
-
-  w#event#connect#key_press ~callback:
-    begin fun ev ->
-      let key = GdkEvent.Key.keyval ev in
-      if key = GdkKeysyms._Escape then w#destroy ();
-      true
-    end;
-
-  GMain.Timeout.add ~ms:20 ~callback:
-    begin fun () ->
-      drawGLScene area (); true
-    end;
-
-  area#connect#display ~callback:(drawGLScene area);
-  area#connect#reshape ~callback:resizeGLScene;
-
-  area#connect#realize ~callback:
-    begin fun () ->
-      initGL ();
-      resizeGLScene ~width ~height
-    end;
-  w#show ();
-
-  w
-let display =
-      let w = createGLWindow "Tutorial 5" 640 480 16 false in
-        GMain.Main.main ()
 
--- fridi.mli
+++ fridi.mli
@@ -1,7 +1,31 @@
 val rtri : float ref
-val rquad : float ref
 val resizeGLScene : width:int -> height:int -> unit
 val initGL : unit -> unit
-val drawGLScene : < swap_buffers : unit -> 'a; .. > -> unit -> 'a
+module VertexMap :
+  sig
+    type key = Int32.t
+    type 'a t = 'a Map.Make(Int32).t
+    val empty : 'a t
+    val is_empty : 'a t -> bool
+    val add : key -> 'a -> 'a t -> 'a t
+    val find : key -> 'a t -> 'a
+    val remove : key -> 'a t -> 'a t
+    val mem : key -> 'a t -> bool
+    val iter : (key -> 'a -> unit) -> 'a t -> unit
+    val map : ('a -> 'b) -> 'a t -> 'b t
+    val mapi : (key -> 'a -> 'b) -> 'a t -> 'b t
+    val fold : (key -> 'a -> 'b -> 'b) -> 'a t -> 'b -> 'b
+    val compare : ('a -> 'a -> int) -> 'a t -> 'a t -> int
+    val equal : ('a -> 'a -> bool) -> 'a t -> 'a t -> bool
+  end
+val drawMap :
+  < swap_buffers : unit -> 'a; .. > ->
+  (Gl.rgb * Gl.point3) VertexMap.t -> VertexMap.key list -> 'a
 val killGLWindow : unit -> unit
-val createGLWindow : string -> int -> int -> int -> 'a -> GWindow.window
+val display :
+  < connect : < display : callback:'a -> 'b;
+                realize : callback:(unit -> unit) -> 'c;
+                reshape : callback:(width:int -> height:int -> unit) -> 'd;
+                .. >;
+    swap_buffers : unit -> 'a; .. > ->
+  int -> int -> (Gl.rgb * Gl.point3) VertexMap.t -> VertexMap.key list -> 'c
