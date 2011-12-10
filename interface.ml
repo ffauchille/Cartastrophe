e51c5ae0f2b1640aad9c0766e8d683f8babfa6bf
Coyf <coyf@slutofcoyf.(none)>
2011-12-10 15:45:18 +0100

    Interface preque resolu

--- interface.ml
+++ interface.ml
@@ -1,27 +1,48 @@
 let width = ref 800
 let height= ref 600
-let namefile= ref "map-simple.png"
-let namefiletreated= ref "map-simple.png"
+let backgroundimage= ref "madamecatastrophe.jpeg"
 let interval = ref 10
+let filenameimage = ref ""
+(*Ouverture de la fenetre*)
 (* On crée la surface d'affichage en doublebuffering *)
 (* let newDisplay () =(Sdlvideo.set_video_mode (!width) (!height) [`DOUBLEBUF]
 let display = ref (newDisplay ())
 let setSize w h = width:=w;height:=h;display :=newDisplay () *)
-let setSize w h = ()
+(*let setSize w h = ()*)
 (* Affiche une image à l'écran *)
 (* let showSurface img = 
 	let d = Sdlvideo.display_format img in
 		Sdlvideo.blit_surface d !display ();
 		Sdlvideo.flip !display
 		 *)
+(*Chargement d'une image *)
+let load_picture = Sdlloader.load_image
+
+let image_processing filename = 
+    let img = load_picture filename in
+(* On récupère les dimensions *)
+    let (w, h) = ImageProcessing.get_dims img in
+(* Traite l'image *)
+    let breaks = (ImageProcessing.detect_areas img) in
+    begin
+    ObjMaker.createObj (filename^".obj") (ObjMaker.calc_intersection (w,h)
+    !interval); 
+    	(* Imprime les bordures sur l'image *)
+    ImageProcessing.print_borders img breaks;
+    	(* Affiche l'image modifiée *)
+    Sdlvideo.save_BMP img (filename^"-traite.bmp");
+    Sdlvideo.save_BMP (ImageProcessing.crisscross img (w,h) (!interval))
+    (filename^"-crisscross.bmp");
+    filenameimage := filename;
+    end		
 let setCaption = Sdlwm.set_caption
+(*string of stringoption*)
 (*Ouverture de la fenetre*)
 let window = GWindow.window
     ~title:"Cartastrophe"
     ~resizable:true
     ~height:!height
     ~width:!width ()
-(*Cadre principal*)
 (*Permettre l'ajout de widgets dans window*)
 let vbox = GPack.vbox
     ~border_width:1
@@ -49,20 +70,45 @@ let frame_visualisation = GBin.frame
     ~label:"Visualisation"
     ~packing:hbox#add ()
 (*Bouton d'aide*)
-let open_button = GFile.chooser_button
-    ~title:"Choix de la carte"
-    ~action:`OPEN
-    ~packing:bbox#add ()
+exception IsNone
+let s_of_s_o = function
+    | Some x -> x
+    | _ -> raise IsNone
+let may_print btn () = Gaux.may image_processing btn#filename
 let image_filter =GFile.filter
     ~name:"Fichier image"
-    ~patterns:["*.bmp";"*.png";"*.jpg";"*.jpeg"]
+    ~patterns:["*.bmp";"*.png";"*.jpg";"*.jpeg"] ()
+let map_button = 
+let button = GFile.chooser_button
+    ~title:"Choix de la carte"
+    ~action:`OPEN
+    (*~set_filter:image_filter*)
+    ~packing:bbox#add () in
+    (button#connect#selection_changed (may_print button));
+GMisc.image ~file:!filenameimage ~packing:frame_image_treated#add();
+    button
+(*let help_message _=
+  let dlg _= GWindow.message_dialog
+    ~message:"1. Cliquez sur le premier bouton, celui permettant de selection 
+    votre image /n 2. Votre image est prétraité et afficher dans le cadre du bas
+    de l'interface /n 3. Vous pouvez nagiver dans la modélisation 3D de votre 
+    image en deux dimensions"
+    ~parent:window
+    ~destroy_with_parent:false
+    ~use_markup:true
+    ~message_type:`QUESTION
+    ~position:`CENTER_ON_PARENT
+    ~buttons:GWindow.Buttons.yes_no () in
+  let res = dlg#run () = `NO in
+  dlg#destroy ();
+  res
 let help =
     let button= GButton.button
 	~stock:`HELP
 	~packing:bbox#add () in
 	GMisc.image ~stock:`HELP ~packing: button#set_image ();
-    (*sw*) (button#connect#clicked ~callback:GMain.quit);
-    button
+    (*sw*) (button#connect#clicked ~callback:help_message);
+    button*)
 let about_button =
   let dlg = GWindow.about_dialog
     ~authors:["Quatrographes (<quatrographes@gmail.com>)"]
@@ -70,7 +116,7 @@ let about_button =
     ~license:"EPITA 2015 "
     ~version:"42.42"
     ~website:"http://cartastro0.wordpress.com/"
-    ~website_label:"Cartastrophe"
+    ~website_label:"Le site Cartastrophe"
     ~position:`CENTER_ON_PARENT
     ~parent:window
     ~destroy_with_parent:true () in
@@ -95,16 +141,17 @@ let quit =
     ~packing: window#add () *)
     
 (*affichage de l'image d'un GMisc.image*)
-let imageview = GMisc.image
-    ~file:!namefiletreated
-    ~packing:frame_image_treated#add ()
-let imagetreatedview = GMisc.image
-    ~file:!namefile
-    ~packing:frame_image_treated#add ()
+(*let imageview = GMisc.image
+    ~file:!filenameimage
+    ~packing:frame_image_treated#add ()*)
 let imagetreated =  GMisc.drawing_area
     ~packing:frame_visualisation#add ()
 (* Suppress warnings *)
 let sw foo = ()
+(*(*image de fond*)
+let background_image = GMisc.image
+    ~file:!backgroundimage
+    ~packing:window#add ()*)
 (* Initialisation SDL,GTK2 et ouverture de la fenetre *)
 let init () = 
 	begin
